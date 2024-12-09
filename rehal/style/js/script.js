892c69a886a1e4a8a3cd59a0c63e5e0f6bcf88d7
amal <amal.m.faqihi@gmail.com>
2024-12-09 20:08:47 +0300

    adding the menu bar

--- rehal/style/js/script.js
+++ rehal/style/js/script.js
@@ -1,1 +1,14 @@
 // use for responsitev desgin and moving object
+
+/////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+/// responseve nav bar ///////////////
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
